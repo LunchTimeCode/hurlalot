99343be804e667d24060d9c57d05a60a52bc6e6f
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2023-06-13 16:49:40 +0200

    feat: remove storage

--- src/app.rs
+++ src/app.rs
@@ -10,10 +10,10 @@ pub struct HApp {
 }
 
 impl HApp {
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
+    pub fn new(_: &eframe::CreationContext<'_>) -> Self {
+        // if let Some(storage) = cc.storage {
+        //     return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        // }
         Default::default()
     }
 }
@@ -26,7 +26,7 @@ impl eframe::App for HApp {
         egui::CentralPanel::default().show(ctx, |ui| self.editor.render(ui));
     }
 
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
-    }
+    // fn save(&mut self, storage: &mut dyn eframe::Storage) {
+    //     eframe::set_value(storage, eframe::APP_KEY, self);
+    // }
 }
